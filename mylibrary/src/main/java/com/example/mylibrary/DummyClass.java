e42f1d5528e86e9e04b7cffd277a46798823a6e4
David Greenberg <gtdavid25@gmail.com>
2018-03-28 10:33:05 +0300

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/DummyClass.java
@@ -0,0 +1,16 @@
+package com.example.mylibrary;
+
+/**
+ * Created by dgreenberg on 27/03/2018.
+ */
+
+public class DummyClass {
+
+    public DummyClass() {
+        System.out.println("Hello Dummy Class New");
+    }
+
+    public void doSom654ething() {
+
+    }
+}
---
+++ mylibrary2/src/main/java/com/example/mylibrary2/DummyClass2.java
@@ -0,0 +1,12 @@
+package com.example.mylibrary2;
+
+/**
+ * Created by dgreenberg on 27/03/2018.
+ */
+
+public class DummyClass2 {
+
+    public DummyClass2() {
+        System.out.println("Hello from Dummy Class 2");
+    }
+}
